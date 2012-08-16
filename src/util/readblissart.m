f43db7723bacdea040da93dd7ff6183bf9280917
Felix Weninger <weninger@tum.de>
2012-08-16 17:20:18 +0200

    updated readblissart.m for tensor structures exported by NMD

--- src/util/readblissart.m
+++ src/util/readblissart.m
@@ -5,12 +5,25 @@ if (nargin < 2)
     precision = 'double';
 end
 type = fread(fp, 1, 'uint32');
-if (type ~= 2)
-    error('not a blissart matrix file')
+if (type == 3)
+    nm = fread(fp, 1, 'uint32');
+else
+    if (type == 2)
+        nm = 1;
+    else
+        error('not a blissart matrix file')
+    end
 end
 rows = fread(fp, 1, 'uint32');
 cols = fread(fp, 1, 'uint32');
-data = fread(fp, [rows, cols], precision);
+if (nm > 1)
+    data = zeros(rows, cols, nm);
+    for mi = 1:nm
+        data(:, :, mi) = fread(fp, [rows, cols], precision);
+    end
+else
+    data = fread(fp, [rows, cols], precision);
+end
 fclose(fp);
 
 end
